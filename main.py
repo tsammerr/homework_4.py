a8f8a65b6a31f8739501354fb343812ba1afe8f5
Maria <tarasovych.maria@gmail.com>
2022-09-30 18:24:34 +0300

    ex 2.1

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 #feature/HM_ex_1
+op = int(input('number of the month -> '))
+
+if op == 1:
+    print('January')
+
+if op == 2:
+    print('February')
+
+if op == 3:
+    print('March')
+
+if op == 4:
+    print('April')
+
+if op == 5:
+    print('May')
+
+if op == 6:
+    print('June')
+
+if op == 7:
+    print('July')
+
+if op == 8:
+    print('August')
+
+if op == 9:
+    print('September')
+
+if op == 10:
+    print('October')
+
+if op == 11:
+    print('November')
+
+if op == 12:
+    print('December')
