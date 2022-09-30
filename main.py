9d7c35c94d4461f9e9faebad966a21f8bd21e802
Maria <tarasovych.maria@gmail.com>
2022-09-30 18:57:51 +0300

    ex 4

---
+++ main.py
@@ -0,0 +1,11 @@
+num_1 = int(input('number 1 -> '))
+num_2 = int(input('number 2 -> '))
+
+if num_1 > num_2:
+    print(f'{num_2} -> {num_1}')
+
+if num_1 < num_2:
+    print(f'{num_1} -> {num_2}')
+
+elif num_1 == num_2:
+    print('The numbers are equal')
