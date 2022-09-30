7f63dbe705ea995435109ee3a59bca6e266d6fec
Maria <tarasovych.maria@gmail.com>
2022-09-30 18:08:44 +0300

    ex 1

---
+++ main.py
@@ -0,0 +1,22 @@
+op = int(input('number of the day -> '))
+
+if op == 1:
+    print('monday')
+
+if op == 2:
+    print('tuesday')
+
+if op == 3:
+    print('wensday')
+
+if op == 4:
+    print('thursday')
+
+if op == 5:
+    print('friday')
+
+if op == 6:
+    print('saturday')
+
+if op == 7:
+    print('sunday')
