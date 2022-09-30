c22de02d78736dd92c13a2c4219675ccfb38b5e6
Maria <tarasovych.maria@gmail.com>
2022-09-30 18:51:29 +0300

    ex 3

---
+++ main.py
@@ -0,0 +1,11 @@
+num = int(input('number -> '))
+
+if num > 0:
+    print('Number is positive')
+
+if num < 0:
+    print('Number is negative')
+
+
+elif num == 0:
+    print('Number is equal to zero')
